9f2dde11eabc80cfe41561906e2bc26a1189f789
kamscode <kamnoah88@gmail.com>
2023-04-06 10:05:09 +0300

    new file added

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
-const listUsers = ["Dev Ed", "Johny", "John Snow", "You"];
+const listUsers = ["Dev Ed", "Johny", "John Snow", "You"];// arrays practice
 
 console.log(listUsers[0]); // arrays practice
+git
---
+++ condtionals.js
@@ -0,0 +1,34 @@
+// == Equal to
+//5==5 True
+//5==4 False
+
+//Not equal to
+//5!=4 True
+//5!=5 False
+
+//5 > 10 False
+//10 > 6 True
+//10 >= 10 True
+// 20 <= 10 False
+
+//NEVER EVER USE DOUBLE EQUALS USE THE TRIPLE
+// const age = 15;
+// const bank = 10;
+
+// if (age >= 18) {
+//   console.log("you have access your old enough");
+//   // s
+// } else if (bank > 30) {
+//   console.log("cool! you have enough money!");
+// } else {
+//   console.log("You are either to young or dont have enough money!");
+// }
+const age = null;
+if (age) {
+  console.log("This value is truthy");
+} else {
+  console.log("This Value is false");
+}
+
+//FALSE,0,"",null,undefined, NaN
+//working with conditions
